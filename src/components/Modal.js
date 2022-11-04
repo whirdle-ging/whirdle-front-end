93187f3ee641ffe8823b82ad8603f4a1fad47da4
Ging Louie <ging.m.louie@gmail.com>
2022-11-04 00:51:17 -0700

    Commit some styliing changes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,14 +12,12 @@ export default function Modal({ isCorrect, solution, turn }) {
 		}
 	});
 
-
-
 	return (
-		<>
 	<div className="modal">
-			if {isCorrect && <div>
-				<h1 id="modal">YOU WON YOUR</h1>
-				<div className="whirdle">
+			{isCorrect && (
+				<div>
+					<h1 id="modal">YOU WON YOUR</h1>
+					<div className="whirdle">
 					<h1 id="modal1" className="animate__animated__animate__rotateOut">W</h1>
 					<h1 id="modal2" className="animate__animated__animate__rotateOut">H</h1>
 					<h1 id="modal3" className="animate__animated__animate__rotateOut">I</h1>
@@ -27,25 +25,25 @@ export default function Modal({ isCorrect, solution, turn }) {
 					<h1 id="modal5" className="animate__animated__animate__rotateOut">D</h1>
 					<h1 id="modal6" className="animate__animated__animate__rotateOut">L</h1>
 					<h1 id="modal7" className="animate__animated__animate__rotateOut">E</h1>
-				</div>
+					</div>
+			
 				<h1 id="modal">MATCH!!!</h1>
-				<p id="modal" className="solution">You solved it! The solution was <span>solution}</span>.</p>
+				<p id="modal" className="solution">You solved it! The solution was <span>{solution}</span>.</p>
 				{turn === 1 ? <div className="solution-line">You found the solution after {turn} guess.</div> : <div className="solution-line">You found the solution after {turn} guesses.</div>}
 				<button className="modal-button" onClick={() => window.location.reload()}>Play Again</button>
-			</div>} else {
+				</div>)} 
 
 			/* Modal when failed whirdle - gives solution, number of turns taken */
-			<div>
-				<h1 id="modal-over">GAME OVER</h1>
-				<h1 id="modal">I AM SORRY. YOU RAN OUT OF TURNS.</h1>
-				
-				<div className="whirdle">
-					<p id="modal" className="solution">The solution was <span>{solution}</span>.</p>
-					<p id="modal" className="solution">Better luck next time :)</p>
+			{!isCorrect && (
+				<div>
+					<h1 id="modal-over">GAME OVER</h1>
+					<h1 id="modal">I AM SORRY. YOU RAN OUT OF TURNS.</h1>
+					<div className="whirdle">
+						<p id="modal" className="solution">The solution was <span>{solution}</span>.</p>
+						<p id="modal" className="solution">Better luck next time :)</p>
+					</div>
+					<button className="modal-button" id="myBtn" onClick={() => window.location.reload()}>Play Again</button>
+				</div>)}
 				</div>
-			
-				<button className="modal-button" id="myBtn" onClick={() => window.location.reload()}>Play Again</button>
-			</div>}
-	</div>;
-		</>)
-		}
+	)
+}
